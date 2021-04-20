26da8edf77ad21c539542cda1d5082d16f39d4ec
shinjunseok <sjsage522@gmail.com>
2021-04-20 18:34:29 +0900

    add : login api
    사용자 컨트롤러 계층 -> 로그인 api 구현

--- src/main/java/io/wisoft/jpashop/controller/api/UserController.java
+++ src/main/java/io/wisoft/jpashop/controller/api/UserController.java
@@ -2,20 +2,36 @@ package io.wisoft.jpashop.controller.api;
 
 import io.wisoft.jpashop.domain.user.Email;
 import io.wisoft.jpashop.domain.user.User;
+import io.wisoft.jpashop.service.UserService;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
+import static io.wisoft.jpashop.controller.api.ApiResult.*;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api")
 public class UserController {
 
+    private final UserService userService;
 
+    @PostMapping("/login")
+    public ApiResult<UserResponse> login(@RequestBody LoginRequest request) {
+        return succeed(
+                new UserResponse(
+                        userService.login(Email.from(request.getEmailAddress()), request.getPassword())
+                )
+        );
+    }
 
     @Getter
     @ToString
@@ -24,7 +40,8 @@ public class UserController {
         private String emailAddress;
         private String password;
 
-        protected LoginRequest() {}
+        protected LoginRequest() {
+        }
 
         public LoginRequest(String emailAddress, String password) {
             this.emailAddress = emailAddress;
@@ -44,7 +61,8 @@ public class UserController {
         private LocalDateTime lastLoginAt;
         private LocalDateTime createAt;
 
-        protected UserResponse() {}
+        protected UserResponse() {
+        }
 
         public UserResponse(User source) {
             BeanUtils.copyProperties(source, this);
