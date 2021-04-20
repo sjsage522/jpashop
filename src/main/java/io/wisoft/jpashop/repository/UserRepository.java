c37fe3deaaed516bf10a8c9916e25cea9f549041
shinjunseok <sjsage522@gmail.com>
2021-04-20 17:53:55 +0900

    add : user repository
    사용자 레포지토리 구현.

---
+++ src/main/java/io/wisoft/jpashop/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package io.wisoft.jpashop.repository;
+
+import io.wisoft.jpashop.domain.user.Email;
+import io.wisoft.jpashop.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Long, User> {
+
+    Optional<User> findByEmail(Email email);
+}
