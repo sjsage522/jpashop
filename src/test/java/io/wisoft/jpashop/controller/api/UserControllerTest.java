98662e1299ea55e8bac2eacf4ca2b9a9da60c9cc
shinjunseok <sjsage522@gmail.com>
2021-04-20 21:32:18 +0900

    add : user test
    사용자 로그인 테스트.

---
+++ src/test/java/io/wisoft/jpashop/controller/api/UserControllerTest.java
@@ -0,0 +1,64 @@
+package io.wisoft.jpashop.controller.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.HashMap;
+
+import static io.wisoft.jpashop.util.JsonUtils.toJson;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("사용자 API 테스트")
+class UserControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    public void setMockMvc(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    @DisplayName("테스트 1. 사용자 로그인 테스트")
+    void _1_userLoginApiTest() throws Exception {
+
+        ResultActions result = mockMvc.perform(
+                post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(
+                                toJson(
+                                    new HashMap<>() {
+                                        {
+                                            put("emailAddress", "test@gmail.com");
+                                            put("password", "1234");
+                                        }
+                                    }
+                                )
+                        )
+        );
+        result.andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.id", is(1)))
+                .andExpect(jsonPath("$.data.name", is("junseok")))
+                .andExpect(jsonPath("$.data.email").isMap())
+                .andExpect(jsonPath("$.data.email['address']", is("test@gmail.com")))
+                .andExpect(jsonPath("$.data.loginCount", is(1)))
+                .andExpect(jsonPath("$.data.lastLoginAt").exists())
+                .andExpect(jsonPath("$.data.createdAt").exists())
+                .andExpect(jsonPath("$.data.errorMessage").doesNotExist());
+    }
+}
