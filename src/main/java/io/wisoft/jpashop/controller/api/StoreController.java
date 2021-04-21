03619719910d7ba183d5ff1de1a046da8c55a3f5
shinjunseok <sjsage522@gmail.com>
2021-04-21 21:10:49 +0900

    add : store api
    정상 영업중인 상점 조회 api 구현.
    
    - fix: StoreResponse -> @Getter, @Setter 추가.

--- src/main/java/io/wisoft/jpashop/controller/api/StoreController.java
+++ src/main/java/io/wisoft/jpashop/controller/api/StoreController.java
@@ -3,15 +3,46 @@ package io.wisoft.jpashop.controller.api;
 import io.wisoft.jpashop.domain.store.BusinessHours;
 import io.wisoft.jpashop.domain.store.Store;
 import io.wisoft.jpashop.domain.store.StoreState;
+import io.wisoft.jpashop.service.StoreService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.BeanUtils;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.wisoft.jpashop.controller.api.ApiResult.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("api")
 public class StoreController {
 
+    private final StoreService storeService;
+
+    @GetMapping("/stores")
+    public ApiResult<List<StoreResponse>> findRunningStore(
+            @RequestParam("time") @DateTimeFormat(pattern = "yyyyMMddHHmmss") LocalDateTime localDateTime) {
+
+        List<Store> runningStores = storeService.findRunningStore(localDateTime);
+        List<StoreResponse> results = new ArrayList<>();
+        for (Store runningStore : runningStores) {
+            StoreResponse storeResponse = new StoreResponse(runningStore);
+            results.add(storeResponse);
+        }
+
+        return succeed(results);
+    }
+
+    @Getter
+    @Setter
     private static class StoreResponse {
 
         private Long id;
