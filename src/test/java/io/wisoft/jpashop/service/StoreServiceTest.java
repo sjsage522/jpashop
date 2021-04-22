3d53692563886091cdf479ab67dbe6b4a6868b24
shinjunseok <sjsage522@gmail.com>
2021-04-22 21:48:28 +0900

    fix : store service test
    정상 영업중인 상점 조회 테스트
    - 단위 테스트를 하도록 변경.

--- src/test/java/io/wisoft/jpashop/service/StoreServiceTest.java
+++ src/test/java/io/wisoft/jpashop/service/StoreServiceTest.java
@@ -1,21 +1,31 @@
 package io.wisoft.jpashop.service;
 
+import io.wisoft.jpashop.domain.store.BusinessHours;
 import io.wisoft.jpashop.domain.store.Store;
+import io.wisoft.jpashop.domain.store.StoreState;
+import io.wisoft.jpashop.repository.StoreRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootTest
+@SpringBootTest(classes = StoreService.class)
 @DisplayName("상점 Service 테스트")
 class StoreServiceTest {
 
-    @Autowired StoreService storeService;
+    @Autowired
+    StoreService storeService;
+
+    @MockBean
+    StoreRepository storeRepository;
 
     @Test
     @DisplayName("테스트 1. 정상 영업중인 상점 조회 테스트")
@@ -24,9 +34,29 @@ class StoreServiceTest {
         LocalDateTime localDateTime = LocalDateTime.parse("2021-03-01 09:00:00"
                 , DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
+        List<Store> stores = createStores();
+        BDDMockito.given(storeRepository.findByState(StoreState.NORMAL))
+            .willReturn(stores);
+
         List<Store> runningStore = storeService.findRunningStore(localDateTime);
 
         Assertions.assertThat(runningStore.size()).isEqualTo(1);
         Assertions.assertThat(runningStore.get(0).getName()).isEqualTo("Normal Store1");
+    }
+
+    private List<Store> createStores() {
+        List<Store> stores = new ArrayList<>();
+
+        BusinessHours businessHours = new BusinessHours();
+        businessHours.setOffDay(7);
+        businessHours.setRun24(false);
+        businessHours.setOpenTime(540);
+        businessHours.setCloseTime(1080);
+
+        stores.add(Store.from(
+                "Normal Store1", StoreState.NORMAL, businessHours
+        ));
+
+        return stores;
     }
 }
