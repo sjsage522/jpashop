e513e9476d360f34b6854eb9d6ded7ae262ec1e1
shinjunseok <sjsage522@gmail.com>
2021-04-22 16:20:32 +0900

    add : favorite store response
    즐겨찾기 상점 응답 dto 구현.

--- src/main/java/io/wisoft/jpashop/controller/api/StoreController.java
+++ src/main/java/io/wisoft/jpashop/controller/api/StoreController.java
@@ -1,5 +1,7 @@
 package io.wisoft.jpashop.controller.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.wisoft.jpashop.domain.favoritestore.FavoriteStore;
 import io.wisoft.jpashop.domain.store.BusinessHours;
 import io.wisoft.jpashop.domain.store.Store;
 import io.wisoft.jpashop.domain.store.StoreState;
@@ -9,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.BeanUtils;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -27,6 +26,11 @@ public class StoreController {
 
     private final StoreService storeService;
 
+    /**
+     * 상점 목록 조회 API
+     * @param localDateTime 영업중인지 판단을 위한 조회 시각
+     * @return 영업 중인 상점 목록
+     */
     @GetMapping("/stores")
     public ApiResult<List<StoreResponse>> findRunningStore(
             @RequestParam("time") @DateTimeFormat(pattern = "yyyyMMddHHmmss") LocalDateTime localDateTime) {
@@ -58,4 +62,29 @@ public class StoreController {
             BeanUtils.copyProperties(source, this);
         }
     }
+
+    @Getter
+    @Setter
+    private static class FavoriteStoreResponse {
+
+        private Long id;
+        private boolean open;
+
+        @JsonProperty("store")
+        private StoreResponse storeResponse;
+
+        private LocalDateTime createdAt;
+
+        public FavoriteStoreResponse(Long id, boolean open, StoreResponse storeResponse, LocalDateTime createdAt) {
+            this.id = id;
+            this.open = open;
+            this.storeResponse = storeResponse;
+            this.createdAt = createdAt;
+        }
+
+        public FavoriteStoreResponse(FavoriteStore source) {
+            BeanUtils.copyProperties(source, this);
+        }
+    }
+
 }
