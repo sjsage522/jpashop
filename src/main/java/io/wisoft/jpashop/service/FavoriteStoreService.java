c740f33420e2960d29c8285b45675f6e38d547a4
shinjunseok <sjsage522@gmail.com>
2021-04-23 20:09:16 +0900

    add : favorite store service layer
    즐겨찾기 상점 서비스 계층 구현.

--- src/main/java/io/wisoft/jpashop/service/FavoriteStoreService.java
+++ src/main/java/io/wisoft/jpashop/service/FavoriteStoreService.java
@@ -1,7 +1,12 @@
 package io.wisoft.jpashop.service;
 
+import com.google.common.base.Preconditions;
 import io.wisoft.jpashop.domain.favoritestore.FavoriteStore;
+import io.wisoft.jpashop.domain.store.Store;
+import io.wisoft.jpashop.domain.user.User;
 import io.wisoft.jpashop.repository.FavoriteStoreRepository;
+import io.wisoft.jpashop.repository.StoreRepository;
+import io.wisoft.jpashop.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,8 +19,21 @@ import java.util.List;
 public class FavoriteStoreService {
 
     private final FavoriteStoreRepository favoriteStoreRepository;
+    private final UserRepository userRepository;
+    private final StoreRepository storeRepository;
 
     public List<FavoriteStore> findByUserId(final Long userId) {
         return favoriteStoreRepository.findByUserId(userId);
+    }
+
+    @Transactional
+    public FavoriteStore addFavoriteStore(final Long userId, final Long storeId) {
+        User findUser = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("user is not exists."));
+        Store findStore = storeRepository.findById(storeId).orElseThrow(() -> new IllegalArgumentException("store is not exists."));
+
+        if (favoriteStoreRepository.findByUserIdAndStoreId(userId, storeId).isPresent())
+            throw new IllegalArgumentException("already exist favorite store.");
+
+        return favoriteStoreRepository.save(FavoriteStore.from(findUser, findStore));
     }
 }
