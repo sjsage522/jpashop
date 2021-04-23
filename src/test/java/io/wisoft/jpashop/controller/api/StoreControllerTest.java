a622c46f698adf7ff4477e87e43494462cb396a2
shinjunseok <sjsage522@gmail.com>
2021-04-23 18:02:11 +0900

    add : store controller api test #4
    즐겨찾기 상점목록 조회 테스트 구현.

--- src/test/java/io/wisoft/jpashop/controller/api/StoreControllerTest.java
+++ src/test/java/io/wisoft/jpashop/controller/api/StoreControllerTest.java
@@ -86,4 +86,40 @@ class StoreControllerTest {
                 .andExpect(jsonPath("$.data.length()", is(0)))
         ;
     }
+
+    @Test
+    @DisplayName("테스트 4. 즐겨찾기 상점목록 조회 테스트 (영업중인 상점은 open 필드가 true, 조회시각 2021-03-01 09:00:00)")
+    void _4_findFavoriteStoreApiTest() throws Exception {
+
+        ResultActions result = mockMvc.perform(
+                get("/api/user/1/bookmark/stores?time=20210301090000")
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data.length()", is(2)))
+                .andExpect(jsonPath("$.data[0].id", is(2)))
+                .andExpect(jsonPath("$.data[0].open", is(false)))
+                .andExpect(jsonPath("$.data[0].store").exists())
+                .andExpect(jsonPath("$.data[0].store.id", is(2)))
+                .andExpect(jsonPath("$.data[0].store.name", is("Hidden Store")))
+                .andExpect(jsonPath("$.data[0].store.storeState", is("HIDDEN")))
+                .andExpect(jsonPath("$.data[0].store.businessHours").exists())
+                .andExpect(jsonPath("$.data[0].store.businessHours.offDay", is(7)))
+                .andExpect(jsonPath("$.data[0].store.businessHours.run24", is(false)))
+                .andExpect(jsonPath("$.data[0].store.businessHours.openTime", is(540)))
+                .andExpect(jsonPath("$.data[0].store.businessHours.closeTime", is(1080)))
+                .andExpect(jsonPath("$.data[1].id", is(1)))
+                .andExpect(jsonPath("$.data[1].open", is(true)))
+                .andExpect(jsonPath("$.data[1].store").exists())
+                .andExpect(jsonPath("$.data[1].store.id", is(1)))
+                .andExpect(jsonPath("$.data[1].store.name", is("Normal Store1")))
+                .andExpect(jsonPath("$.data[1].store.storeState", is("NORMAL")))
+                .andExpect(jsonPath("$.data[1].store.businessHours").exists())
+                .andExpect(jsonPath("$.data[1].store.businessHours.offDay", is(7)))
+                .andExpect(jsonPath("$.data[1].store.businessHours.run24", is(false)))
+                .andExpect(jsonPath("$.data[1].store.businessHours.openTime", is(540)))
+                .andExpect(jsonPath("$.data[1].store.businessHours.closeTime", is(1080)));
+    }
 }
