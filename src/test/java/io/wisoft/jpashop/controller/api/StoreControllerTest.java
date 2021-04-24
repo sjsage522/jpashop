df8d93ee16bc017d51f8c8ddb508775fe14f99b9
shinjunseok <sjsage522@gmail.com>
2021-04-24 19:33:07 +0900

    add : store controller api test #5
    즐겨찾기 상점 추가 성공 테스트 구현.

--- src/test/java/io/wisoft/jpashop/controller/api/StoreControllerTest.java
+++ src/test/java/io/wisoft/jpashop/controller/api/StoreControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -121,5 +122,47 @@ class StoreControllerTest {
                 .andExpect(jsonPath("$.data[1].store.businessHours.run24", is(false)))
                 .andExpect(jsonPath("$.data[1].store.businessHours.openTime", is(540)))
                 .andExpect(jsonPath("$.data[1].store.businessHours.closeTime", is(1080)));
+    }
+
+    @Test
+    @DisplayName("테스트 5. 즐겨찾기 상점 추가 성공 테스트")
+    void _5_addFavoriteStoreApiTest() throws Exception {
+
+        ResultActions result = mockMvc.perform(
+                post("/api/user/1/store/3/bookmark")
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.store.id", is(3)))
+                .andExpect(jsonPath("$.data.store.name", is("Normal Store2")))
+                .andExpect(jsonPath("$.data.store.storeState", is("NORMAL")))
+                .andExpect(jsonPath("$.data.store.businessHours").exists())
+                .andExpect(jsonPath("$.data.store.businessHours.offDay", is(1)))
+                .andExpect(jsonPath("$.data.store.businessHours.run24", is(true)))
+                .andExpect(jsonPath("$.data.store.businessHours.openTime", is(0)))
+                .andExpect(jsonPath("$.data.store.businessHours.closeTime", is(0)))
+                .andExpect(jsonPath("$.data.createdAt").exists())
+        ;
+        // 즐겨찾기 추가 후 목록 조회
+        ResultActions after = mockMvc.perform(
+                get("/api/user/1/bookmark/stores?time=20210301230000")
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+        after.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data.length()", is(3)))
+                // 첫번째 즐겨찾기 상점
+                .andExpect(jsonPath("$.data[0].id", is(3)))
+                .andExpect(jsonPath("$.data[0].store.name", is("Normal Store2")))
+                // 두번째 즐겨찾기 상점
+                .andExpect(jsonPath("$.data[1].id", is(2)))
+                .andExpect(jsonPath("$.data[1].store.name", is("Hidden Store")))
+                // 세번째 즐겨찾기 상점
+                .andExpect(jsonPath("$.data[2].id", is(1)))
+                .andExpect(jsonPath("$.data[2].store.name", is("Normal Store1")))
+        ;
     }
 }
