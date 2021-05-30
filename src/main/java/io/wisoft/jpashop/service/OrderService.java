7ed1138a578038fa5f8b5d7e3c6fdae24e0406a5
shinjunseok <sjsage522@gmail.com>
2021-05-30 18:28:55 +0900

    update : orderService
    주문 서비스 계층 구현

--- src/main/java/io/wisoft/jpashop/service/OrderService.java
+++ src/main/java/io/wisoft/jpashop/service/OrderService.java
@@ -1,7 +1,15 @@
 package io.wisoft.jpashop.service;
 
 import io.wisoft.jpashop.domain.order.Order;
+import io.wisoft.jpashop.domain.order.OrderState;
+import io.wisoft.jpashop.domain.orderitem.OrderItem;
+import io.wisoft.jpashop.domain.store.Store;
+import io.wisoft.jpashop.domain.user.User;
+import io.wisoft.jpashop.repository.OrderItemRepository;
 import io.wisoft.jpashop.repository.OrderRepository;
+import io.wisoft.jpashop.repository.StoreRepository;
+import io.wisoft.jpashop.repository.UserRepository;
+import io.wisoft.jpashop.util.StoreUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,8 +23,27 @@ import java.util.List;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final StoreRepository storeRepository;
+    private final UserRepository userRepository;
+    private final OrderItemRepository orderItemRepository;
 
     public List<Order> findAll(Long userId, LocalDateTime from, LocalDateTime to) {
         return orderRepository.findAll(userId, from, to);
+    }
+
+    @Transactional
+    public Order order(Long userId, Long storeId, LocalDateTime time, List<OrderItem> orderItems) {
+
+        Store findStore = storeRepository.findById(storeId).orElseThrow();
+        if (!StoreUtils.isNormalBusinessHours(time, findStore.getBusinessHours()))
+            throw new IllegalStateException("영업중 상점이 아님");
+
+        User findUser = userRepository.findById(userId).orElseThrow();
+
+        Order newOrder = orderRepository.save(Order.from(findStore, findUser, orderItems, OrderState.NEW));
+
+        orderItemRepository.saveAll(orderItems);
+
+        return newOrder;
     }
 }
