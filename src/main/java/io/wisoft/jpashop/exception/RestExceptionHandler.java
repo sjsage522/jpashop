b3441839b81b68fc7c087cf4edd79b94ff545efc
shinjunseok <sjsage522@gmail.com>
2021-05-30 18:30:21 +0900

    update : rest exception handler
    예외 추가

--- src/main/java/io/wisoft/jpashop/exception/RestExceptionHandler.java
+++ src/main/java/io/wisoft/jpashop/exception/RestExceptionHandler.java
@@ -1,9 +1,12 @@
 package io.wisoft.jpashop.exception;
 
 import io.wisoft.jpashop.controller.api.ApiResult;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice("io.wisoft.jpashop.controller")
 public class RestExceptionHandler {
@@ -12,5 +15,22 @@ public class RestExceptionHandler {
     @ResponseBody
     protected ApiResult<Object> illegalArgument(final IllegalArgumentException ex) {
         return ApiResult.failed(ex.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseBody
+    protected ApiResult<Object> noSuchElement(final NoSuchElementException ex) {
+        return ApiResult.failed(ex.getMessage());
+    }
+
+    @ExceptionHandler(BindException.class)
+    @ResponseBody
+    protected ApiResult<Object> bindException(
+            final BindException ex) {
+        return ApiResult.failed(ex
+                .getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage());
     }
 }
