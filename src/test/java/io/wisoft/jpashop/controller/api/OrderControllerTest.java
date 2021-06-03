d626c415349cdc8e6363555928178375f1967f30
shinjunseok <sjsage522@gmail.com>
2021-06-03 13:55:36 +0900

    update : order api test
    주문 취소 api 테스트코드 작성

--- src/test/java/io/wisoft/jpashop/controller/api/OrderControllerTest.java
+++ src/test/java/io/wisoft/jpashop/controller/api/OrderControllerTest.java
@@ -17,6 +17,7 @@ import java.util.HashMap;
 
 import static io.wisoft.jpashop.util.JsonUtils.toJson;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -37,7 +38,7 @@ class OrderControllerTest {
 
     @Test
     @DisplayName("테스트 01. 주문목록 조회 테스트 (조회시각 2021-03-01 00:00:00 ~ 2021-03-02 00:00:00)")
-    void _1_findOrdersApiTest() throws Exception {
+    void _01_findOrdersApiTest() throws Exception {
         ResultActions result = mockMvc.perform(
                 MockMvcRequestBuilders.get("/api/user/1/orders/between/20210301000000/and/20210302000000")
                         .accept(MediaType.APPLICATION_JSON)
@@ -79,8 +80,8 @@ class OrderControllerTest {
     }
 
     @Test
-    @DisplayName("테스트 2. 주문하기 성공 테스트")
-    void _2_newOrderApiTest() throws Exception {
+    @DisplayName("테스트 02. 주문하기 성공 테스트")
+    void _02_newOrderApiTest() throws Exception {
         ResultActions result = mockMvc.perform(
                 post("/api/user/1/store/3/order?time=20210302090000")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -123,6 +124,68 @@ class OrderControllerTest {
                 .andExpect(jsonPath("$.data.state", is("NEW")))
                 .andExpect(jsonPath("$.data.cancel").doesNotExist())
                 .andExpect(jsonPath("$.data.completedAt").doesNotExist())
+        ;
+    }
+
+    @Test
+    @DisplayName("테스트 03. 주문취소 실패 테스트 (취소불가 상태)")
+    void _03_orderCancelApiTest() throws Exception {
+        ResultActions result = mockMvc.perform(
+                patch("/api/user/1/order/3/cancel")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(
+                                toJson(
+                                        new HashMap<>() {
+                                            {
+                                                put("message", "expensive price");
+                                            }
+                                        }
+                                )
+                        )
+        );
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").doesNotExist())
+                .andExpect(jsonPath("$.errorMessage").exists())
+        ;
+    }
+
+    @Test
+    @DisplayName("테스트 04. 주문취소 성공 테스트")
+    void _04_orderCancelApiTest() throws Exception {
+        ResultActions result = mockMvc.perform(
+                patch("/api/user/1/order/1/cancel")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(
+                                toJson(new HashMap<>() {
+                                    {
+                                        put("message", "expensive price");
+                                    }
+                                })
+                        )
+        );
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.id", is(1)))
+                .andExpect(jsonPath("$.data.storeId", is(1)))
+                .andExpect(jsonPath("$.data.items").isArray())
+                .andExpect(jsonPath("$.data.items.length()", is(2)))
+                .andExpect(jsonPath("$.data.items[0].name", is("MENU-A")))
+                .andExpect(jsonPath("$.data.items[0].unitPrice", is(10000)))
+                .andExpect(jsonPath("$.data.items[0].unitCount", is(2)))
+                .andExpect(jsonPath("$.data.items[1].name", is("MENU-B")))
+                .andExpect(jsonPath("$.data.items[1].unitPrice", is(5000)))
+                .andExpect(jsonPath("$.data.items[1].unitCount", is(3)))
+                .andExpect(jsonPath("$.data.totalPrice", is(35000)))
+                .andExpect(jsonPath("$.data.state", is("CANCEL")))
+                .andExpect(jsonPath("$.data.cancel").exists())
+                .andExpect(jsonPath("$.data.cancel.message", is("expensive price")))
+                .andExpect(jsonPath("$.data.cancel.time").exists())
+                .andExpect(jsonPath("$.data.completedAt").doesNotExist())
+                .andExpect(jsonPath("$.errorMessage").doesNotExist())
         ;
     }
 }
